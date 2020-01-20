10d230ff9426c6a81016e2630381e29e111816d3
Jakub Dakowski <jakubdakowski@gmail.com>
2020-01-20 21:10:45 +0100

    Działa

--- templatka.py
+++ templatka.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 
+from psychopy import visual, event, core
 import multiprocessing as mp
 import pygame as pg
-import numpy as np
 import pandas as pd
 import filterlib as flt
 import blink as blk
-import random
-import time
 #from pyOpenBCI import OpenBCIGanglion
+import time
+
 
 def blinks_detector(quit_program, blink_det, blinks_num, blink,):
     def detect_blinks(sample):
@@ -58,59 +58,13 @@ def blinks_detector(quit_program, blink_det, blinks_num, blink,):
         board = OpenBCIGanglion(mac=mac_adress)
         board.start_stream(detect_blinks)
 
-# Text rendering
-
-def text_objects(text, font):
-    textSurface = font.render(text, True, (255,255,255))
-    return textSurface, textSurface.get_rect()
-
-def message_display(text):
-    largeText = pg.font.Font('freesansbold.ttf',60)
-    TextSurf, TextRect = text_objects(text, largeText)
-    TextRect.center = (45,30)
-    screen.blit(TextSurf, TextRect)
-
-# Gamer stuff
-
-def sort_key(obj, win=120):
-    return abs(obj.x-win)
-
-def managebeats(container, tetno, fps=60):
-    if container[0].x<-60:
-        container.pop(0)
-    if container[-1].x<720-(tetno*fps/180):
-        container.append(Beat(container))
-
-class Beat(object):
-    
-    def __init__(self, parent):
-        self.y = 240
-        self.x = 800
-        self.parent = parent
-
-    def move(self, tetno, fps=60):
-        self.x -= tetno/fps*5
-
-    def get_obj(self):
-        return pg.Rect(self.x, self.y, 20, 60)
-
-    def get_points(self, wykrycie = 60, mod=0.1):
-        return mod*(wykrycie-abs(self.x - 120))
-
-    def destroy(self, wykrycie = 20):
-        if len(self.parent)<=2:
-            return self.get_points(wykrycie=wykrycie)
-        else:
-            self.parent.pop(self.parent.index(self))
-            return self.get_points(wykrycie=wykrycie)
-
-
 if __name__ == "__main__":
-    pg.init()
+
+
     blink_det = mp.Queue()
     blink = mp.Value('i', 0)
     blinks_num = mp.Value('i', 0)
-    connected = mp.Event()
+    #connected = mp.Event()
     quit_program = mp.Event()
 
     proc_blink_det = mp.Process(
@@ -126,6 +80,53 @@ if __name__ == "__main__":
     ############################################
     # Poniżej należy dodać rozwinięcie programu
     ############################################
+    pg.init()
+
+# Text rendering
+
+    def text_objects(text, font):
+        textSurface = font.render(text, True, (255,255,255))
+        return textSurface, textSurface.get_rect()
+
+    def message_display(text):
+        largeText = pg.font.Font('freesansbold.ttf',60)
+        TextSurf, TextRect = text_objects(text, largeText)
+        TextRect.center = (45,30)
+        screen.blit(TextSurf, TextRect)
+
+    # Gamer stuff
+
+    def sort_key(obj, win=120):
+        return abs(obj.x-win)
+
+    def managebeats(container, tetno, fps=60):
+        if container[0].x<-60:
+            container.pop(0)
+        if container[-1].x<720-(tetno*fps/180):
+            container.append(Beat(container))
+
+    class Beat(object):
+        
+        def __init__(self, parent):
+            self.y = 240
+            self.x = 800
+            self.parent = parent
+
+        def move(self, tetno, fps=60):
+            self.x -= tetno/fps*5
+
+        def get_obj(self):
+            return pg.Rect(self.x, self.y, 20, 60)
+
+        def get_points(self, wykrycie = 60, mod=0.1):
+            return mod*(wykrycie-abs(self.x - 120))
+
+        def destroy(self, wykrycie = 20):
+            if len(self.parent)<=2:
+                return self.get_points(wykrycie=wykrycie)
+            else:
+                self.parent.pop(self.parent.index(self))
+                return self.get_points(wykrycie=wykrycie)
 
     screen = pg.display.set_mode((720, 480))
     done = False
@@ -170,6 +171,5 @@ if __name__ == "__main__":
 
         clock.tick(60)
 
-
-    # Zakończenie podprocesów
-        proc_blink_det.join()
+# Zakończenie podprocesów
+    proc_blink_det.join()
